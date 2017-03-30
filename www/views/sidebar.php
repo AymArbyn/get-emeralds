2093de70cff79c2cc27e03e5ed4059264614bcd2
AymArbyn <arbyn.acosta@gmail.com>
2017-03-30 11:28:55 +0800

    Create sidebar.php

---
+++ www/views/sidebar.php
@@ -0,0 +1,68 @@
+<div id="quick-info">
+    <div class="container-fluid sidebar-container">
+        <header>
+            <h5><?= $title ?></h5>
+        </header>
+
+        <a href="#" id="dc1" class="overview-link disabled selected">
+            <figure class="graph-container">
+                <div id="dc1-graph" class="graph"></div>
+                <span class="graph-label">DC1</span>
+                <div class="graph-value">
+                    <span class="graph-exact">Turned Off</span>
+                    <span class="graph-percentage">0%</span>
+                </div>
+            </figure>
+        </a>
+        <a href="#" id="dc2" class="overview-link disabled">
+            <figure class="graph-container">
+                <div id="dc2-graph" class="graph"></div>
+                <span class="graph-label">DC2</span>
+                <div class="graph-value">
+                    <span class="graph-exact">Turned Off</span>
+                    <span class="graph-percentage">0%</span>
+                </div>
+            </figure>
+        </a>
+        <a href="#" id="dc3" class="overview-link disabled">
+            <figure class="graph-container">
+                <div id="dc3-graph" class="graph"></div>
+                <span class="graph-label">DC3</span>
+                <div class="graph-value">
+                    <span class="graph-exact">Turned Off</span>
+                    <span class="graph-percentage">0%</span>
+                </div>
+            </figure>
+        </a>
+        <a href="#" id="dc4" class="overview-link disabled">
+            <figure class="graph-container">
+                <div id="dc4-graph" class="graph"></div>
+                <span class="graph-label">DC4</span>
+                <div class="graph-value">
+                    <span class="graph-exact">Turned Off</span>
+                    <span class="graph-percentage">0%</span>
+                </div>
+            </figure>
+        </a>
+        <a href="#" id="ac1" class="overview-link disabled">
+            <figure class="graph-container">
+                <div id="ac1-graph" class="graph"></div>
+                <span class="graph-label">AC1</span>
+                <div class="graph-value">
+                    <span class="graph-exact">Turned Off</span>
+                    <span class="graph-percentage">0%</span>
+                </div>
+            </figure>
+        </a>
+        <a href="#" id="ac2" class="overview-link disabled">
+            <figure class="graph-container">
+                <div id="ac2-graph" class="graph"></div>
+                <span class="graph-label">AC2</span>
+                <div class="graph-value">
+                    <span class="graph-exact">Turned Off</span>
+                    <span class="graph-percentage">0%</span>
+                </div>
+            </figure>
+        </a>
+    </div>
+</div>
